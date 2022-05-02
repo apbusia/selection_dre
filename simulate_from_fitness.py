73e8c33970e9e51b6843a6c2e1a1b4cea3cf2df7
Akosua Busia <akosua@berkeley.edu>
2022-05-02 13:53:31 -0700

    Adds quantile normalization option

--- simulate_from_fitness.py
+++ simulate_from_fitness.py
@@ -7,6 +7,16 @@ import data_prep
 SEED = 7
 
 
+def quantile_normalization(df):
+    # https://stackoverflow.com/questions/37935920/quantile-normalization-on-pandas-dataframe
+    ranks = df.rank(method='first').stack()
+    rank_mean = df.stack().groupby(ranks).mean()
+    # Add interpolated values in between ranks.
+    finer_ranks = ((rank_mean.index+0.5).to_list() + rank_mean.index.to_list())
+    rank_mean = rank_mean.reindex(finer_ranks).sort_index().interpolate()
+    return df.rank(method='average').stack().map(rank_mean).unstack()
+
+
 def main(args):
     np.random.seed(SEED)
     
@@ -16,7 +26,18 @@ def main(args):
     print('Using {} as fitness'.format(args.fitness_column))
     df = df[['seq', args.fitness_column]]
     fitness = df[args.fitness_column].values
-    if args.normalize:
+    if args.q_norm_file is not None:
+        print('Applying quantile normalization using log-enrichments from {}'.format(args.q_norm_file))
+        counts_df = pd.read_csv(args.q_norm_file)
+        pre_counts = counts_df['count_pre'] + 1
+        post_counts = counts_df['count_post'] + 1
+        del counts_df
+        enrich_scores = data_prep.calculate_enrichment_scores(pre_counts, post_counts, pre_counts.sum(), post_counts.sum())[:, 0]
+        qn_df = pd.DataFrame(np.column_stack((fitness, enrich_scores)), columns = ['fitness','enrich_scores'])
+        qn_df = quantile_normalization(qn_df)
+        fitness = qn_df['fitness']
+    if args.standardize:
+        print('Standardizing fitness to have 0 mean and unit variance...')
         fitness = (fitness - np.mean(fitness)) / np.std(fitness)
     
     print('Computing \'true\' library proportions...')
@@ -56,7 +77,8 @@ if __name__ == '__main__':
     parser.add_argument('--dirichlet_concentration', default=1, help='(scalar) concentration parameter for Dirichlet dist', type=float)
     parser.add_argument('--n_replicates', default=1, help='number of replicates of count data to generate', type=int)
     parser.add_argument('--shuffle', help='save data in shuffled order', action='store_true')
-    parser.add_argument('--normalize', help='normalize fitness_column before simulating library proportions', action='store_true')
+    parser.add_argument('--q_norm_file', help='path to counts dataset to use to quantile normalize fitness_column', type=str)
+    parser.add_argument('--standardize', help='standardize fitness_column before simulating library proportions', action='store_true')
     parser.add_argument('--save_file', help='output path to which to save generated counts', type=str)
     args = parser.parse_args()
     main(args)
