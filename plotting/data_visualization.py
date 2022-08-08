ffb12d6712c9541f9d50c3143bd7f5b3b01520c6
Akosua Busia <akosua@berkeley.edu>
2022-08-08 12:28:58 -0700

    Updates data histograms

--- plotting/data_visualization.py
+++ plotting/data_visualization.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from model_comparison import set_rc_params
@@ -13,25 +14,50 @@ def main(args):
     
     df = pd.read_csv(args.data_file)
     
-    fig, axes = plt.subplots(1, 4, figsize=(20, 4))
+    fig, axes = plt.subplots(3, 1, figsize=(3, 9))
+    palette = sns.color_palette('crest', n_colors=1) + sns.color_palette('flare', n_colors=1)
     
-    # Fitness distribution
-    ax = axes[0]
-    sns.histplot(data=df, x=args.fitness_column, kde=True, ax=ax)
+#     # Fitness distribution
+#     ax = axes[0]
+#     sns.histplot(data=df, x=args.fitness_column, stat='density', kde=True, color=palette[0], ax=ax)
     
     # Enrichment distribution
+    ax = axes[0]
+    sns.histplot(data=df, x=args.enrichment_column, stat='density', color=palette[0], ax=ax)
+    ax.set_xlabel('Groundtruth Log-enrichment')
+    
     ax = axes[1]
-    sns.histplot(data=df, x=args.enrichment_column, kde=True, ax=ax)
+    pre_counts = df[args.pre_column] + 1
+    post_counts = df[args.post_column] + 1
+    df['observed'] = np.log((post_counts / np.sum(post_counts)) / (pre_counts / np.sum(pre_counts)))
+#     sns.histplot(data=df[['groundtruth', 'observed']], stat='density', kde=True, common_norm=False, palette=palette, ax=ax)
+    sns.histplot(data=df, x='observed', stat='density', color=palette[0], ax=ax)
+    ax.set_xlabel('Observed Log-enrichment')
     
-    # Pre count distribution
+    # Count distributions
     ax = axes[2]
-    df[args.pre_column] = df[args.pre_column] + 1  # Add pseudocount for log-scale
-    sns.histplot(data=df, x=args.pre_column, log_scale=True, ax=ax)
-    
-    # Post count distribution
-    ax = axes[3]
-    df[args.post_column] = df[args.post_column] + 1  # Add pseudocount for log-scale
-    sns.histplot(data=df, x=args.post_column, log_scale=True, ax=ax)
+    df[args.pre_column] = df[args.pre_column] + 1 * (df[args.pre_column] == 0)  # Add pseudocount for log-scale
+    df[args.post_column] = df[args.post_column] + 1 * (df[args.post_column] == 0) # Add pseudocount for log-scale
+    df = df.rename(columns={args.pre_column: 'pre', args.post_column: 'post'})
+    sns.histplot(data=df[['pre', 'post']], stat='proportion', multiple='dodge', log_scale=True, common_norm=False, palette=palette, ax=ax)
+#     sns.histplot(data=df[['pre', 'post']], stat='proportion', element='step', cumulative=True, fill=False, common_norm=False, palette=palette, ax=ax)
+#     ax.set_ylim(0, 1)
+    ax.set_xlabel('Sequencing Count')
+
+#     m = df['post'].max()
+#     g = sns.JointGrid(data=df, x='pre', y='post', height=3) #, xlim=(0, m), ylim=(0, m), marginal_ticks=True)
+#     # g.ax_joint.set(xscale='log', yscale='log')
+# #     cax = g.figure.add_axes([.15, .55, .02, .2])
+#     g.plot_joint(sns.histplot, cmap=sns.light_palette(palette[0], as_cmap=True)) #, cbar=True, cbar_ax=cax)
+#     g.plot_marginals(sns.histplot, element='step', stat='proportion', color=palette[0])
+#     g.set_axis_labels('Pre Count', 'Post Count')
+
+#     g = sns.JointGrid(data=df, x='groundtruth', y='observed', height=3) #, xlim=(0, m), ylim=(0, m), marginal_ticks=True)
+#     # g.ax_joint.set(xscale='log', yscale='log')
+# #     cax = g.figure.add_axes([.15, .55, .02, .2])
+#     g.plot_joint(sns.histplot, cmap=sns.light_palette(palette[0], as_cmap=True)) #, cbar=True, cbar_ax=cax)
+#     g.plot_marginals(sns.histplot, element='step', stat='density', color=palette[0])
+#     g.set_axis_labels('Groundtruth Log-enrichment', 'Observed Log-enrichment')
     
     plt.savefig(os.path.join(out_dir, '{}_data_visualization.png'.format(out_tag)), dpi=300, transparent=False, bbox_inches='tight', facecolor='white')
     plt.close()
